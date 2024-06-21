28f14114a0b8e206a9335fc250f5aeb141c68b9c
Kashyap surani <152683782+Kashyapsurani@users.noreply.github.com>
2024-06-21 13:56:25 +0530

    Grop Project-2

---
+++ script.js
@@ -0,0 +1,47 @@
+function bary() {
+    document.querySelector(".right").style.rotate = "1deg";
+    document.querySelector(".right").style.transition = "1s ease";
+    document.querySelector(".right").style.backgroundColor = "#FF71A4";
+    document.querySelector("#Star").src = "img/Star 1 (2).png";
+    document.querySelector("#Star").style.transition = "1s ease";
+    document.querySelector(".stro").src = "img/image 2 (3).png";
+    document.querySelector(".stro").style.transition = "1s ease";
+    document.querySelector("body").style.background = "#F3B0D6"
+}
+
+function banana() {
+    document.querySelector(".right").style.rotate = "-89deg";
+    document.querySelector(".right").style.transition = "1s ease";
+    document.querySelector(".right").style.backgroundColor = "#FCB300";
+    document.querySelector("#Star").src = "img/Star 2.png";
+    document.querySelector("#Star").style.transition = "1s ease";
+    document.querySelector(".stro").src = "img/istockphoto-1324093361-612x612 1.png";
+    document.querySelector(".stro").style.transition = "1s ease";
+    document.querySelector("body").style.background = "#FFD367"
+}
+
+
+function keye() {
+    document.querySelector(".right").style.rotate = "-176deg";
+    document.querySelector(".right").style.transition = "1s ease";
+    document.querySelector(".right").style.backgroundColor = "#97D039";
+    document.querySelector("#Star").src = "img/Star 3.png";
+    document.querySelector("#Star").style.transition = "1s ease";
+    document.querySelector(".stro").src = "img/kiwi vector.png";
+    document.querySelector(".stro").style.transition = "1s ease";
+    document.querySelector("body").style.background = "#C2EA7E "
+
+}
+
+function dryfrute() {
+    document.querySelector(".right").style.rotate = "-275deg";
+    document.querySelector(".right").style.transition = "1s ease";
+    document.querySelector(".right").style.backgroundColor = "#4A9CF4";
+    document.querySelector("#Star").src = "img/Star 4.png";
+    document.querySelector("#Star").style.transition = "1s ease";
+    document.querySelector(".stro").src = "img/Blue Berry.png";
+    document.querySelector(".stro").style.transition = "1s ease";
+    document.querySelector("body").style.background = "#8DB9F4"
+
+}
+
